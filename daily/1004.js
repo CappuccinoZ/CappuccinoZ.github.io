4ef946f42ff657e1162c1f7f785027263474f60d
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2021-02-07 18:23:33 +0800

    commit message

---
+++ daily/1004.js
@@ -0,0 +1,160 @@
+document.write(`<table>
+<tbody>
+<tr>
+<td style="background:#a0ffa0"><sub>1</sub>H<br>氢<br>1.008</td>
+<td colspan="16">
+<h2>元素周期表</h2>
+</td>
+<td style="background:#c0ffff"><sub>2</sub>He<br>氦<br>4.003</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>3</sub>Li<br>锂<br>6.941</td>
+<td style="background:#ffdead"><sub>4</sub>Be<br>铍<br>9.012</td>
+<td>&nbsp;</td>
+<td style="background-color:#ff6666">碱金属</td>
+<td style="background-color:#ffdead">碱土<br>金属</td>
+<td style="background-color:#ffc0c0">过渡<br>金属</td>
+<td style="background-color:#cccccc">后过渡<br>金属</td>
+<td style="background-color:#cccc99">类金属</td>
+<td style="background-color:#ffff99">卤族<br>元素</td>
+<td style="background-color:#c0ffff">稀有<br>气体</td>
+<td style="background-color:#a0ffa0">其他<br>非金属</td>
+<td>&nbsp;</td>
+<td style="background:#cccc99"><sub>5</sub>B<br>硼<br>10.81</td>
+<td style="background:#a0ffa0"><sub>6</sub>C<br>碳<br>12.011</td>
+<td style="background:#a0ffa0"><sub>7</sub>N<br>氮<br>14.007</td>
+<td style="background:#a0ffa0"><sub>8</sub>O<br>氧<br>15.999</td>
+<td style="background:#ffff99"><sub>9</sub>F<br>氟<br>18.998</td>
+<td style="background:#c0ffff"><sub>10</sub>Ne<br>氖<br>20.180</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>11</sub>Na<br>钠<br>22.990</td>
+<td style="background:#ffdead"><sub>12</sub>Mg<br>镁<br>24.305</td>
+<td colspan="10">&nbsp;</td>
+<td style="background:#cccccc"><sub>13</sub>Al<br>铝<br>26.982</td>
+<td style="background:#cccc99"><sub>14</sub>Si<br>硅<br>28.085</td>
+<td style="background:#a0ffa0"><sub>15</sub>P<br>磷<br>30.974</td>
+<td style="background:#a0ffa0"><sub>16</sub>S<br>硫<br>32.065</td>
+<td style="background:#ffff99"><sub>17</sub>Cl<br>氯<br>35.453</td>
+<td style="background:#c0ffff"><sub>18</sub>Ar<br>氩<br>39.948</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>19</sub>K<br>钾<br>39.098</td>
+<td style="background:#ffdead"><sub>20</sub>Ca<br>钙<br>40.078</td>
+<td style="background:#ffc0c0"><sub>21</sub>Sc<br>钪<br>44.956</td>
+<td style="background:#ffc0c0"><sub>22</sub>Ti<br>钛<br>47.867</td>
+<td style="background:#ffc0c0"><sub>23</sub>V<br>钒<br>50.942</td>
+<td style="background:#ffc0c0"><sub>24</sub>Cr<br>铬<br>51.996</td>
+<td style="background:#ffc0c0"><sub>25</sub>Mn<br>锰<br>54.938</td>
+<td style="background:#ffc0c0"><sub>26</sub>Fe<br>铁<br>55.845</td>
+<td style="background:#ffc0c0"><sub>27</sub>Co<br>钴<br>58.933</td>
+<td style="background:#ffc0c0"><sub>28</sub>Ni<br>镍<br>58.693</td>
+<td style="background:#ffc0c0"><sub>29</sub>Cu<br>铜<br>63.546</td>
+<td style="background:#ffc0c0"><sub>30</sub>Zn<br>锌<br>65.38</td>
+<td style="background:#cccccc"><sub>31</sub>Ga<br>镓<br>69.723</td>
+<td style="background:#cccc99"><sub>32</sub>Ge<br>锗<br>72.630</td>
+<td style="background:#cccc99"><sub>33</sub>As<br>砷<br>74.922</td>
+<td style="background:#a0ffa0"><sub>34</sub>Se<br>硒<br>78.971</td>
+<td style="background:#ffff99"><sub>35</sub>Br<br>溴<br>79.904</td>
+<td style="background:#c0ffff"><sub>36</sub>Kr<br>氪<br>83.798</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>37</sub>Rb<br>铷<br>85.468</td>
+<td style="background:#ffdead"><sub>38</sub>Sr<br>锶<br>87.62</td>
+<td style="background:#ffc0c0"><sub>39</sub>Y<br>钇<br>88.906</td>
+<td style="background:#ffc0c0"><sub>40</sub>Zr<br>锆<br>91.224</td>
+<td style="background:#ffc0c0"><sub>41</sub>Nb<br>铌<br>92.906</td>
+<td style="background:#ffc0c0"><sub>42</sub>Mo<br>钼<br>95.95</td>
+<td style="background:#ffc0c0"><sub>43</sub>Tc<br>锝<br>[98]</td>
+<td style="background:#ffc0c0"><sub>44</sub>Ru<br>钌<br>101.07</td>
+<td style="background:#ffc0c0"><sub>45</sub>Rh<br>铑<br>102.91</td>
+<td style="background:#ffc0c0"><sub>46</sub>Pd<br>钯<br>106.42</td>
+<td style="background:#ffc0c0"><sub>47</sub>Ag<br>银<br>107.87</td>
+<td style="background:#ffc0c0"><sub>48</sub>Cd<br>镉<br>112.41</td>
+<td style="background:#cccccc"><sub>49</sub>In<br>铟<br>114.82</td>
+<td style="background:#cccccc"><sub>50</sub>Sn<br>锡<br>118.71</td>
+<td style="background:#cccc99"><sub>51</sub>Sb<br>锑<br>121.76</td>
+<td style="background:#cccc99"><sub>52</sub>Te<br>碲<br>127.60</td>
+<td style="background:#ffff99"><sub>53</sub>I<br>碘<br>126.90</td>
+<td style="background:#c0ffff"><sub>54</sub>Xe<br>氙<br>131.29</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>55</sub>Cs<br>铯<br>132.91</td>
+<td style="background:#ffdead"><sub>56</sub>Ba<br>钡<br>137.33</td>
+<td style="background:#ffbfff">镧系<br>元素</td>
+<td style="background:#ffc0c0"><sub>72</sub>Hf<br>铪<br>178.49</td>
+<td style="background:#ffc0c0"><sub>73</sub>Ta<br>钽<br>180.95</td>
+<td style="background:#ffc0c0"><sub>74</sub>W<br>钨<br>183.84</td>
+<td style="background:#ffc0c0"><sub>75</sub>Re<br>铼<br>186.21</td>
+<td style="background:#ffc0c0"><sub>76</sub>Os<br>锇<br>190.23</td>
+<td style="background:#ffc0c0"><sub>77</sub>Ir<br>铱<br>192.22</td>
+<td style="background:#ffc0c0"><sub>78</sub>Pt<br>铂<br>195.08</td>
+<td style="background:#ffc0c0"><sub>79</sub>Au<br>金<br>196.97</td>
+<td style="background:#ffc0c0"><sub>80</sub>Hg<br>汞<br>200.59</td>
+<td style="background:#cccccc"><sub>81</sub>Tl<br>铊<br>204.38</td>
+<td style="background:#cccccc"><sub>82</sub>Pb<br>铅<br>207.2</td>
+<td style="background:#cccccc"><sub>83</sub>Bi<br>铋<br>208.98</td>
+<td style="background:#cccc99"><sub>84</sub>Po<br>钋<br>[209]</td>
+<td style="background:#ffff99"><sub>85</sub>At<br>砹<br>[210]</td>
+<td style="background:#c0ffff"><sub>86</sub>Rn<br>氡<br>[222]</td>
+</tr>
+<tr>
+<td style="background:#ff6666"><sub>87</sub>Fr<br>钫<br>[223]</td>
+<td style="background:#ffdead"><sub>88</sub>Ra<br>镭<br>[226]</td>
+<td style="background:#ff99cc">锕系<br>元素</td>
+<td style="background:#ffc0c0"><sub>104</sub>Rf<br>𬬻<br>[267]</td>
+<td style="background:#ffc0c0"><sub>105</sub>Db<br>𬭊<br>[268]</td>
+<td style="background:#ffc0c0"><sub>106</sub>Sg<br>𬭳<br>[269]</td>
+<td style="background:#ffc0c0"><sub>107</sub>Bh<br>𬭛<br>[270]</td>
+<td style="background:#ffc0c0"><sub>108</sub>Hs<br>𬭶<br>[277]</td>
+<td style="background:#ffc0c0"><sub>109</sub>Mt<br>鿏<br>[276]</td>
+<td style="background:#ffc0c0"><sub>110</sub>Ds<br>𫟼<br>[281]</td>
+<td style="background:#ffc0c0"><sub>111</sub>Rg<br>𬬭<br>[282]</td>
+<td style="background:#ffc0c0"><sub>112</sub>Cn<br>鿔<br>[285]</td>
+<td style="background:#ececec"><sub>113</sub>Nh<br>鿭<br>[286]</td>
+<td style="background:#ececec"><sub>114</sub>Fl<br>𫓧<br>[289]</td>
+<td style="background:#ececec"><sub>115</sub>Mc<br>镆<br>[289]</td>
+<td style="background:#ececec"><sub>116</sub>Lv<br>鉝<br>[293]</td>
+<td style="background:#ececec"><sub>117</sub>Ts<br>鿬<br>[294]</td>
+<td style="background:#ececec"><sub>118</sub>Og<br>鿫<br>[294]</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td style="background:#ffbfff">镧系<br>元素</td>
+<td style="background:#ffbfff"><sub>57</sub>La<br>镧<br>138.91</td>
+<td style="background:#ffbfff"><sub>58</sub>Ce<br>铈<br>140.12</td>
+<td style="background:#ffbfff"><sub>59</sub>Pr<br>镨<br>140.91</td>
+<td style="background:#ffbfff"><sub>60</sub>Nd<br>钕<br>144.24</td>
+<td style="background:#ffbfff"><sub>61</sub>Pm<br>钷<br>[145]</td>
+<td style="background:#ffbfff"><sub>62</sub>Sm<br>钐<br>150.36</td>
+<td style="background:#ffbfff"><sub>63</sub>Eu<br>铕<br>151.96</td>
+<td style="background:#ffbfff"><sub>64</sub>Gd<br>钆<br>157.25</td>
+<td style="background:#ffbfff"><sub>65</sub>Tb<br>铽<br>158.93</td>
+<td style="background:#ffbfff"><sub>66</sub>Dy<br>镝<br>162.50</td>
+<td style="background:#ffbfff"><sub>67</sub>Ho<br>钬<br>164.93</td>
+<td style="background:#ffbfff"><sub>68</sub>Er<br>铒<br>167.26</td>
+<td style="background:#ffbfff"><sub>69</sub>Tm<br>铥<br>168.93</td>
+<td style="background:#ffbfff"><sub>70</sub>Yb<br>镱<br>173.05</td>
+<td style="background:#ffbfff"><sub>71</sub>Lu<br>镥<br>174.97</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td style="background:#ff99cc">锕系<br>元素</td>
+<td style="background:#ff99cc"><sub>89</sub>Ac<br>锕<br>[227]</td>
+<td style="background:#ff99cc"><sub>90</sub>Th<br>钍<br>232.04</td>
+<td style="background:#ff99cc"><sub>91</sub>Pa<br>镤<br>231.04</td>
+<td style="background:#ff99cc"><sub>92</sub>U<br>铀<br>238.03</td>
+<td style="background:#ff99cc"><sub>93</sub>Np<br>镎<br>[237]</td>
+<td style="background:#ff99cc"><sub>94</sub>Pu<br>钚<br>[244]</td>
+<td style="background:#ff99cc"><sub>95</sub>Am<br>镅<br>[243]</td>
+<td style="background:#ff99cc"><sub>96</sub>Cm<br>锔<br>[247]</td>
+<td style="background:#ff99cc"><sub>97</sub>Bk<br>锫<br>[247]</td>
+<td style="background:#ff99cc"><sub>98</sub>Cf<br>锎<br>[251]</td>
+<td style="background:#ff99cc"><sub>99</sub>Es<br>锿<br>[252]</td>
+<td style="background:#ff99cc"><sub>100</sub>Fm<br>镄<br>[257]</td>
+<td style="background:#ff99cc"><sub>101</sub>Md<br>钔<br>[258]</td>
+<td style="background:#ff99cc"><sub>102</sub>No<br>锘<br>[259]</td>
+<td style="background:#ff99cc"><sub>103</sub>Lr<br>铹<br>[262]</td>
+</tr>
+</tbody>
+</table>`)
