76e6b5caa43ca5999d5a635457055cba30d51f80
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2021-05-21 23:18:05 +0800

    Update duration.js

--- js/duration.js
+++ js/duration.js
@@ -1,7 +1,7 @@
 !(function () {
     var now = new Date();
     var grt = new Date("2019-12-07 0:0:00");
-    days = Math.floor((now - grt) / 86400000) + 1;
+    days = Math.ceil((now - grt) / 86400000);
     if (days % 10 == 1 && days % 100 != 11) s = "st";
     else if (days % 10 == 2 && days % 100 != 12) s = "nd";
     else if (days % 10 == 3 && days % 100 != 13) s = "rd";
