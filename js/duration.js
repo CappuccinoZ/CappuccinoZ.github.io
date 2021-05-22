5a85bca0ce58b614a1b16982f217fc015681d7be
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2021-05-22 22:44:54 +0800

    Update duration.js

--- js/duration.js
+++ js/duration.js
@@ -2,9 +2,9 @@
     var now = new Date();
     var grt = new Date("2019/12/07 00:00:00");
     days = Math.ceil((now - grt) / 86400000);
-    if (days % 10 == 1 && days % 100 != 11) s = "st";
-    else if (days % 10 == 2 && days % 100 != 12) s = "nd";
-    else if (days % 10 == 3 && days % 100 != 13) s = "rd";
-    else s = "th";
+    s = days % 10 == 1 && days % 100 != 11 ? "st"
+        : days % 10 == 2 && days % 100 != 12 ? "nd"
+            : days % 10 == 3 && days % 100 != 13 ? "rd"
+                : "th";
     document.getElementById("timeDate").innerHTML = "The " + days + s + " Day";
 })();
