4c9827a8815e7debe02daf883f990a3657564437
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2021-05-22 14:22:55 +0800

    update 2021-5-22

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function () {
     var now = new Date();
-    var grt = new Date("2019-12-07 0:0:00");
+    var grt = new Date("2019/12/07 00:00:00");
     days = Math.ceil((now - grt) / 86400000);
     if (days % 10 == 1 && days % 100 != 11) s = "st";
     else if (days % 10 == 2 && days % 100 != 12) s = "nd";
